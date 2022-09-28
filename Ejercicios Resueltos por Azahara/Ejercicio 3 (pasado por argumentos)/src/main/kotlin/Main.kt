e4ca0efdf656302861ddbb824082d7004afd520e
Azahara Blanco Rodriguez <blancaluna92@hotmail.com>
2022-09-28 08:32:03 +0200

    ejercicio a medias 3.3

--- Ejercicios Resueltos por Azahara/Ejercicio 3 (pasado por argumentos)/src/main/kotlin/Main.kt
+++ Ejercicios Resueltos por Azahara/Ejercicio 3 (pasado por argumentos)/src/main/kotlin/Main.kt
@@ -1,56 +1,32 @@
-import java.io.File
-import java.nio.file.Files
-import java.nio.file.Files.exists
-import java.nio.file.Path
-import java.nio.file.Paths
+import java.io.*
 import java.util.*
 
 
-
 fun main(args: Array<String>) {
-    //cojemos el directorio donde esta nuestro jar
+
+ //cojemos el directorio donde esta nuestro jar
     var path = System.getProperty("user.dir")+
             File.separator + ".idea"+
             File.separator + "jar"+
             File.separator + "consoleApp.jar"
     //creamos el comando a ejecutar
-    var comando = "java -jar $path 6"
+    var comando = "java -jar $path"
 
     //creamos proceso
      var proceso = Runtime.getRuntime().exec(comando)
-     proceso.waitFor()
-     proceso.destroy()
 
+    // enviamos al proceso texto como "se la metemos" es output
 
+    val out: OutputStream = proceso.outputStream
+    val pw = PrintWriter(OutputStreamWriter(out))
+    pw.println("5")
+    pw.close()
+    
 
+    proceso.waitFor()
+    proceso.destroy()
 
-
-
-
-/*
-Subprograma del jar
-
-  var tiempo : Int? = args[0].toIntOrNull()
-    if(tiempo!=null){
-        for(i in 0 until tiempo!!){
-            var pathOriginal = System.getProperty("user.dir") +
-                    File.separator + ".idea" +
-                    File.separator + "textoOriginal"+
-                    File.separator + "texto.txt"
-            var pathCopy = System.getProperty("user.dir") +
-                    File.separator + ".idea" +
-                    File.separator + "textoCopy" +
-                    File.separator + "textoNumero${i}"
-            if(Files.exists(Path.of(pathOriginal))){
-                println("el fichero $pathOriginal exixte")
-                Files.copy(Path.of(pathOriginal),Path.of( pathCopy))
-                Thread.sleep(10000)
-            }else{
-                println("el fichero $pathOriginal no exixte")
-            }
-
-        }
     }
- */
 
-}
+
+
